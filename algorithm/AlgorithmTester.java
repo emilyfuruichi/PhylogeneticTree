a2663412fe430cec5a0a0e9b0f9a7b833b334385
Emily <60939799+emilyfuruichi@users.noreply.github.com>
2020-05-11 12:08:00 -0700

    Updated to include new tests

--- algorithm/AlgorithmTester.java
+++ algorithm/AlgorithmTester.java
@@ -1,11 +1,16 @@
+/**
+ * Tester for our program. It calls the UPGMA algorithm while
+ * giving it the data files. All the work is done internally.
+ */
+
 package algorithm;
 
 public class AlgorithmTester 
 {
 	public static void main(String[] args)
 	{
-		UPGMA test = new UPGMA();
+		UPGMA test = new UPGMA("lectureData");
 		
-		
+		UPGMA test2 = new UPGMA("HW4Data");
 	}
 }
