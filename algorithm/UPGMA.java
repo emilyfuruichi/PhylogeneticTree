e4a950a97146c976e80f15863275c3b219cebf0b
Emily <60939799+emilyfuruichi@users.noreply.github.com>
2020-05-11 12:09:57 -0700

    updated to work as expected
    computes UPGMA non-graphical tree. Graphical implementation to be added

--- algorithm/UPGMA.java
+++ algorithm/UPGMA.java
@@ -1,3 +1,11 @@
+/**
+ * UPGMA uses the unweighted pair group method using arithmetic averages
+ * to create a phylogenetic tree. Each progressive step down in the distance
+ * matrix is written to a new file for easy viewing. The final tree is 
+ * currently displayed as a non-graphical representation of the tree as we
+ * ran out of time to complete the graphical representation.
+ */
+
 package algorithm;
 
 import java.io.FileNotFoundException;
@@ -10,22 +18,27 @@ public class UPGMA
 	private FileHandling fh;
 	private int count;
 	private Point min;
+	private static boolean DEBUG = false; //set to true if troubleshooting algorithm
+	private String filename;
 	
-	public UPGMA()
+	/**
+	 * Creates a tree using UPGMA from the given file
+	 * @param fileToUse file containing the distance matrix
+	 */
+	public UPGMA(String fileToUse)
 	{
 		try 
 		{	
-			//test filehandling
+			filename = fileToUse;
 			fh = new FileHandling();
 			count = 0;
-			array = fh.readFromCSV("lectureData.csv");
-			lineRepOfTree = "(";
-			//fh.writeToFile("test", array);
-			//findMin();
+			array = fh.readFromCSV(filename);
 			
+			printArray();
 			runAlgorithm();
+			lineRepOfTree = min.getCluster();
 			
-			//fh.writeToFile("output", array);
+			System.out.println("\n"+filename+" Tree: "+lineRepOfTree);
 		} 
 		catch (FileNotFoundException e) 
 		{
@@ -33,18 +46,27 @@ public class UPGMA
 		}
 	}
 	
+	/**
+	 * Controls how many times the matrix is shrunk
+	 * Loops until there is only 1 column of distance values
+	 */
 	public void runAlgorithm()
 	{
-		while(array.size() > 2 && count < 2)
+		while(array.size() > 2)
 		{
 			findMin();
 			redoMatrix();
 		}
 	}
 	
+	/**
+	 * Finds the minimum distance in the matrix
+	 * @return minimum point
+	 */
 	public Point findMin()
 	{
-		System.out.println("\nfindMin");
+		if(DEBUG)
+			System.out.println("\nfindMin");
 		Point temp;
 		Point minFound = null;
 		
@@ -62,22 +84,30 @@ public class UPGMA
 				}
 			}
 		}
-		//System.out.println("minFound: "+minFound+"\n");
+		
 		min = minFound;
 		return minFound;
 	}
 
+	/**
+	 * Takes the distance matrix in array and puts it through another iteration of the UPGMA
+	 * computation so it combines another cluster based on the minimum point/distance
+	 */
 	public void redoMatrix()
 	{
-		System.out.println("redoMatrix min: " + min);
+		if(DEBUG)
+		{
+			System.out.println("redoMatrix min: " + min);
+		}
 		ArrayList<ArrayList<Point>> newArray = new ArrayList<ArrayList<Point>>();
-		Double valNew, val1, val2, val3, val4; //points to sum and average goes in pNew
+		Double valNew, val1, val2, val3, val4;
 		Point tempPoint;
+		String newCluster;
 		
-		for(int i = 0; i < array.size(); i++) //create a new array 1 size smaller
+		for(int i = 0; i < array.size()-1; i++) //create a new array 1 size smaller
 		{
 			newArray.add(new ArrayList<Point>());
-			for(int j = 0; j < array.size(); j++)
+			for(int j = 0; j < array.size()-1; j++)
 			{
 				newArray.get(i).add(new Point());
 			}
@@ -85,105 +115,148 @@ public class UPGMA
 		
 		for(int row = 0, newRows = 0; row <= array.size(); row++)
 		{
-			if(row != min.getRow() && row != min.getCol())
+			while(row == min.getRow() || row == min.getCol())
 			{
-				for(int col = 0, newCols = 0; col <= array.size(); col++)
+				row++;
+			}
+			for(int col = 0, newCols = 0; col <= array.size(); col++)
+			{
+				while(col == min.getCol() || col == min.getRow())
 				{
-					if(col != min.getCol() && col != min.getRow())
+					col++;
+				}
+				if(DEBUG)
+				{
+					System.out.println("\narray row: "+row+" col: "+col);
+					System.out.println("newArrary row: "+newRows+" col: "+newCols);
+				}
+				
+				if( row == 0) //clusters horizontal
+				{
+					if(col != min.getCol() && col != min.getRow() && col <= newArray.size()) //copy from parent matrix
 					{
-						System.out.println("\narray row: "+row+" col: "+col);
-						System.out.println("newArrary row: "+newRows+" col: "+newCols);
 						
-						if( row == 0 ) //cluster title horizontal
+						newCluster = array.get(row).get(col).getCluster();
+						tempPoint = new Point(newCols,newRows,newCluster,true);
+						if(DEBUG)
 						{
-							if(col == array.size())//create new cluster
-							{
-								System.out.println("create new cluster horizontal header col at end");
-								String newCluster = array.get(0).get(min.getCol()).getCluster()+""+array.get(0).get(min.getRow()).getCluster();
-								tempPoint = new Point(newCols,newRows,newCluster,true);
-								System.out.println("         tempPoint in new cluster "+tempPoint);
-								newArray.get(0).set(newCols,tempPoint);
-								newCols++;
-								newRows++;
-							}
-							else if(col != min.getCol() && col != min.getRow()) //copy from parent matrix
-							{
-								System.out.println("copy cluster horizontal title");
-								String newCluster = array.get(row).get(col).getCluster();
-								tempPoint = new Point(newCols,newRows,newCluster,true);
-								System.out.println("         tempPoint in copied cluster "+tempPoint);
-								newArray.get(0).set(newCols,tempPoint);
-								newCols++;
-							}
+							System.out.println("copy cluster horizontal title");
+							System.out.println("         tempPoint in copied cluster "+tempPoint);
 						}
-						else if(col == 0 && row > 0) //cluster title vertical
-						{
-							System.out.println("cluster title vertical");
-							String newCluster = newArray.get(0).get(newRows).getCluster();
-							tempPoint = new Point(newCols,newRows,newCluster,true);
-							System.out.println("         tempPoint in vertical cluster "+tempPoint);
-							newArray.get(newRows).set(0,tempPoint);
-							newCols++;
-						}
-						else if(row >= col) //diagonal zeros, identities o
-						{
-							System.out.println("set cell to 0");
-							tempPoint = new Point (newCols,newRows,0.0,null,false);
-							System.out.println("         tempPoint in 0 cell "+tempPoint);
-							newArray.get(newRows).set(newCols,tempPoint);
-							newCols++;
-						}
-						else if(row != array.size() && col != array.size())
-						{
-							newArray.get(newRows).set(newCols,array.get(row).get(col));
-							System.out.println("         copy cell with val " + newArray.get(newRows).get(newCols).getValue());
-							newCols++;
-						}
+						newArray.get(0).set(newCols,tempPoint);
+						newCols++;
 					}
-					if(col >= array.size() && row < array.size())
+					else //create new cluster horizontal
 					{
-						val1 = array.get(row).get(min.getCol()).getValue();
-						val2 = array.get(min.getRow()).get(row).getValue();
-						val3 = array.get(min.getCol()).get(row).getValue();
-						val4 = array.get(row).get(min.getRow()).getValue();
-						valNew = ( val1+val2+val3+val4 ) / 2;
-						tempPoint = new Point(newCols,newRows,valNew,newArray.get(0).get(newRows).getCluster(),false);
-						newArray.get(newRows).set(newCols,tempPoint);
-						System.out.println("         computed new cluster val:("+val1+"+"+val2+"+"+val3+"+"+val4+")/2="+valNew);
+						newCluster = "("+array.get(0).get(min.getCol()).getCluster()+" "+array.get(0).get(min.getRow()).getCluster()+")";
+						tempPoint = new Point(newCols,newRows,newCluster,true);
+						if(DEBUG)
+						{	
+							System.out.println("create new cluster horizontal header col at end");
+							System.out.println("         tempPoint in new cluster "+tempPoint);
+						}
+						newArray.get(0).set(newCols,tempPoint);
 						newCols++;
 						newRows++;
 					}
 				}
+				else if(col == 0 && row > 0) //clusters vertical
+				{
+					newCluster = newArray.get(0).get(newRows).getCluster();
+					tempPoint = new Point(newCols,newRows,newCluster,true);
+					if(DEBUG)
+					{
+						System.out.println("cluster title vertical");
+						System.out.println("         tempPoint in vertical cluster "+tempPoint);
+					}
+					newArray.get(newRows).set(0,tempPoint);
+					newCols++;
+				}
+				else if(row >= col) //0s
+				{
+					tempPoint = new Point (newCols,newRows,0.0,null,false);
+					if(DEBUG)
+					{
+						System.out.println("set cell to 0");
+						System.out.println("         tempPoint in 0 cell "+tempPoint);
+					}
+					newArray.get(newRows).set(newCols,tempPoint);
+					newCols++;
+				}
+				else if(row <= newArray.size() && col <= newArray.size()) //copy
+				{
+					newCluster = newArray.get(0).get(newCols).getCluster()+newArray.get(0).get(newRows).getCluster();
+					tempPoint = new Point(newCols,newRows,array.get(row).get(col).getValue(),newCluster,false);
+					newArray.get(newRows).set(newCols,tempPoint);
+					if(DEBUG)
+					{
+						System.out.println("copy cell with same value");
+						System.out.println("         copy cell with val " + newArray.get(newRows).get(newCols).getValue());
+					}
+					newCols++;
+				}
+				else if(col > newArray.size() && row <= newArray.size() && row != 0) //compute new cluster col
+				{
+					val1 = array.get(row).get(min.getCol()).getValue();
+					val2 = array.get(min.getRow()).get(row).getValue();
+					val3 = array.get(min.getCol()).get(row).getValue();
+					val4 = array.get(row).get(min.getRow()).getValue();
+					valNew = ( val1+val2+val3+val4 ) / 2;
+					newCluster = "("+newArray.get(0).get(newCols).getCluster()+" "+newArray.get(0).get(newRows).getCluster()+")";
+					tempPoint = new Point(newArray.size()-1,newRows,valNew,newCluster,false);
+					newArray.get(newRows).set(newArray.size()-1,tempPoint);
+					if(DEBUG)
+					{
+						System.out.println("         computed new cluster val:("+val1+"+"+val2+"+"+val3+"+"+val4+")/2="+valNew);
+					}
+					newCols++;
+					newRows++;
+				}
 			}
-			if(row == array.size()) //new cluster calc
+			if(row == array.size()) //new cluster
 			{
-				System.out.println("final row new");
-				String newCluster = array.get(0).get(min.getRow()).getCluster()+array.get(0).get(min.getCol()).getCluster();
+				newCluster = newArray.get(0).get(newArray.size()-1).getCluster();
 				tempPoint = new Point(array.size()-2,array.size()-2,0.0,newCluster,false);
-				System.out.println("         tempPoint in new bottom corner cell "+tempPoint);
+				if(DEBUG)
+				{
+					System.out.println("final row new");
+					System.out.println("         tempPoint in new bottom corner cell "+tempPoint);
+				}
 				newArray.get(array.size()-2).set(array.size()-2,tempPoint);
 				newRows++;
 			}
-			
-			
 		}
+		
 		array = newArray;
-		printArray(newArray);
-		//fh.writeToFile("output"+count, newArray);
-		System.out.println("finished "+ ++count);
-		System.out.println("----------------------------------------------");
+		++count;
+		
+		if(DEBUG)
+		{
+			System.out.println("finished "+ count);
+			System.out.println("-------------------------------------------");
+		}
+		
+		//printArray();
+		
+		fh.writeToFile(filename+"_output_"+count, newArray);
 	}
 	
-	private void printArray(ArrayList<ArrayList<Point>> arr)
+	/**
+	 * Prints the current array to the console for ease of access
+	 * (same array is also written to csv file and viewable there)
+	 */
+	private void printArray()
 	{
-		for(ArrayList<Point> i : arr)
+		System.out.println("\nPrinting current Array");
+		for(ArrayList<Point> i : array)
 		{
 			for(Point j : i)
 			{
 				if(j.isHeader)
-					System.out.print(j.getCluster()+",  ");
+					System.out.print(j.getCluster()+"\t");
 				else
-					System.out.print(j.getValue()+",");
+					System.out.printf("%.3f,\t",j.getValue());
+				//System.out.print(j+",\t");
 			}
 			System.out.println();
 		}
