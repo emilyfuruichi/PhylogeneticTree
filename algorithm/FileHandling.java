cd94d36fd64c22e50e95709c33e1bf6b8475557a
Emily <60939799+emilyfuruichi@users.noreply.github.com>
2020-05-11 12:08:30 -0700

    updated to restructure output

--- algorithm/FileHandling.java
+++ algorithm/FileHandling.java
@@ -1,6 +1,6 @@
 /**
  * FileHandling class can read csv files.
- * Read data is stored in an array list of array lists of Strings
+ * Read data is stored in an array list of array lists of Points
  * for the algorithm to use. This way a multitude of sequences of 
  * various sizes can be read without specifying length or number
  */
@@ -17,6 +17,9 @@ public class FileHandling
 {
 	private ArrayList<ArrayList<Point>> array; //to hold distance matrix
 	
+	/**
+	 * Creates a new array to fill
+	 */
 	public FileHandling()
 	{
 		array = new ArrayList<ArrayList<Point>>();
@@ -24,13 +27,13 @@ public class FileHandling
 	
 	/**
 	 * Given a csv file with the distance matrix, we want to read it into
-	 * an arraylist of arraylists of Strings for the algorithm
+	 * an arraylist of arraylists of Points for the algorithm
 	 * @param fileName the name of the data file
 	 * @throws FileNotFoundException file does not exist
 	 */
 	public ArrayList<ArrayList<Point>> readFromCSV(String fileName) throws FileNotFoundException
 	{
-		File inputFile = new File(fileName);
+		File inputFile = new File(fileName+".csv");
 		int rowCount = 0;
 		
 		try (Scanner sc = new Scanner(inputFile, "UTF-8")) //"data.csv"
@@ -51,9 +54,9 @@ public class FileHandling
 	}
 	
 	/**
-	 * Helper method splits up the given string into an arraylist of Strings
+	 * Helper method splits up the given string into an arraylist of Points
 	 * @param nextline the string to split up by comma
-	 * @return the arraylist of Strings in the row
+	 * @return the arraylist of Points in the row
 	 */
 	private ArrayList<Point> readNextRow(String nextline, int rowCount) 
 	{
@@ -74,7 +77,8 @@ public class FileHandling
 	        	}
 	        	else
 	        	{
-	        		String clusterName = array.get(0).get(colCount).getCluster()+array.get(0).get(rowCount).getCluster();
+	        		//System.out.println(rowCount + " " + colCount + " "+array.get(0).size());
+	        		String clusterName = "("+array.get(0).get(colCount).getCluster()+" "+array.get(0).get(rowCount).getCluster()+")";
 	        		newPoint = new Point(rowCount,colCount,Double.parseDouble(next),clusterName,false);
 	        	}
 	        	colVals.add(newPoint);
